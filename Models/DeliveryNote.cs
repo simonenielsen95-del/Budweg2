aca6e8ed3b7b70b6fba5351c4e30d0a02e248ec5
Corinna Haustrup <corinnahaustrup@gmail.com>
2026-03-11 14:31:51 +0100

    deliverynote properties

---
+++ Models/DeliveryNote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Budweg2._0.Models
+{
+    public class DeliveryNote
+    {
+        public int OrderNo { get; set; }
+        public int StartQuantity { get; set; }
+        public int ItemNo { get; set; }
+    }
+}
