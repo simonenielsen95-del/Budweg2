5206ffff4f93a340a1dffe524bb8809e62c8915c
Simone Nielsen <simonenielsen95@gmail.com>
2026-03-13 14:14:03 +0100

    random new stuff i guess

--- VievModels/MainWindowViewModel.cs
+++ VievModels/MainWindowViewModel.cs
@@ -3,22 +3,37 @@ using System.Collections.Generic;
 using System.Text;
 using Budweg2._0.Repository;
 using Budweg2._0.Models;
+using System.Collections.ObjectModel;
 
 namespace Budweg2._0.VievModels
 {
     internal class MainWindowViewModel
     {
+        private DeliveryNoteRepository deliveryNoteRepository = new DeliveryNoteRepository();
+
         public int ItemNo { get; set; }
 
         public int Quantity { get; set; }
 
+        public ObservableCollection<DeliveryNote>  OCDeliveryNotes { get; set; } = new ObservableCollection<DeliveryNote>();
 
 
-        public DeliveryNote CreateNewDeliveryNote()
+        public MainWindowViewModel() 
         { 
-            DeliveryNote newDeliveryNote = new DeliveryNote();
 
-            return newDeliveryNote;
+            foreach(DeliveryNote dn in deliveryNoteRepository.GetAllDeliveryNotes())
+            {
+                OCDeliveryNotes.Add(dn);
+            }
+        
         }
+
+
+        //public DeliveryNote CreateNewDeliveryNote()
+        //{ 
+        //    DeliveryNote newDeliveryNote = new DeliveryNote();
+
+        //    return newDeliveryNote;
+        //}
     }
 }
